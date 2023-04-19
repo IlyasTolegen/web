b8527f85267cef9061ca6da2ac655e3fffc03372
IlyasTolegen <124000608+IlyasTolegen@users.noreply.github.com>
2023-04-19 20:33:17 +0600

    lab10

---
+++ MODELSS.py
@@ -0,0 +1,90 @@
+@api_view(['GET', 'POST'])
+def company_list(request):
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def company_detail(request, pk):
+    company = get_object_or_404(Company, pk=pk)
+
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CompanySerializer(company, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Class-based views for Company model
+
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+# Function-based views for Vacancy model
+
+@api_view(['GET', 'POST'])
+def vacancy_list(request):
+    if request.method == 'GET':
+        vacancies = Vacancy.objects.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = VacancySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def vacancy_detail(request, pk):
+    vacancy = get_object_or_404(Vacancy, pk=pk)
+
+    if request.method == 'GET':
+        serializer = VacancySerializer(vacancy)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = VacancySerializer(vacancy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        vacancy.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Class-based views for Vacancy model
+
+class VacancyList(generics.ListCreateAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class VacancyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
--- urls.py
+++ urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
-from api.views import CompanyList, CompanyDetail, CompanyVacanciesList, VacancyList, VacancyDetail, TopTenVacanciesList
+from .views import CompanyList, CompanyDetail, CompanyVacanciesList, VacancyList, VacancyDetail, TopTenVacanciesList
 
 urlpatterns = [
     path('api/companies/', CompanyList.as_view(), name='company-list'),
-    path('api/companies/<int:pk>/', CompanyDetail.as_view(),
+    path('api/companies/<int:pk>/', CompanyDetail.as_view(), name='company-detail'),
+    path('api/companies/<int:id>/vacancies/', CompanyVacanciesList.as_view(), name='company-vacancies'),
+    path('api/vacancies/', VacancyList.as_view(), name='vacancy-list'),
+]
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import generics, status
+
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
